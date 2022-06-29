9354ef3f6d6e8bbb96b821a5f5a32879a6ee26c7
exslym <76844562+exslym@users.noreply.github.com>
2022-06-29 14:33:51 +0300

    React.memo added - MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,11 @@
 // @ts-nocheck
-import React from 'react';
+import React, { memo } from 'react';
 import Post from './Post/Post';
 import styles from './MyPosts.module.scss';
 import { AddNewPostReduxForm } from './AddPostForm/AddPostForm';
 
-const MyPosts = props => {
+const MyPosts = memo(props => {
+	console.log('RENDER');
 	let postElements = props.posts.map(p => (
 		<Post message={p.post} count={p.likesCount} key={p.id} />
 	));
@@ -20,6 +21,6 @@ const MyPosts = props => {
 			<div className={styles.posts}>{postElements}</div>
 		</>
 	);
-};
+});
 
 export default MyPosts;
