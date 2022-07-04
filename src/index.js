7404978ffe320916882ce33e756671f484122f94
exslym <76844562+exslym@users.noreply.github.com>
2022-07-04 15:40:20 +0300

    HashRouter added instead of BrowserRouter in index.js, favicon path fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
 import './index.css';
@@ -19,11 +19,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 );
 
