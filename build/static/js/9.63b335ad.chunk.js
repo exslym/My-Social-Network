0f90a23e38d98cb3c0e732084c44e4fd0034372f
exslym <76844562+exslym@users.noreply.github.com>
2022-07-04 14:16:33 +0300

    deploy

---
+++ build/static/js/9.63b335ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[9],{2009:function(e,n,s){s.r(n),s.d(n,{default:function(){return a}});s(2791);var p="News_app_news__zFFjB",u=s(184),a=function(){return(0,u.jsx)("div",{className:p,children:(0,u.jsx)("p",{children:"News"})})}}}]);
+//# sourceMappingURL=9.63b335ad.chunk.js.map
